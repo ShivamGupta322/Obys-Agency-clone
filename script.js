c4a095c7a9f76327ec8eb41b78c4f8533d0c0f62
ShivamGupta322 <guptashivamsg02@gmail.com>
2024-07-16 13:03:47 +0530

    first page done

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-
+function loadingAnimation(){
+    
 var tl=gsap.timeline()
 tl.from(".line h1",{
     y:150,
@@ -25,7 +26,7 @@ tl.from("#line1-part-1, .line h2",{
 tl.to("#loader",{
     opacity:0,
     duration:0.4,
-    delay:4
+    delay:0 
 })
 
 tl.from("#page1",{
@@ -37,3 +38,24 @@ tl.from("#page1",{
 tl.to("#loader",{
     display:"none"
 })
+tl.from("#nav",{
+    opacity:0
+})
+tl.from("#hero1 h1,#hero2 h1,#hero3 h2,#hero4 h1",{
+    y:120,
+    stagger:0.2
+})
+}
+loadingAnimation();
+
+function cursorAnimation(){
+    document.addEventListener("mousemove",function(dets){
+        gsap.to("#crsr",{
+            left:dets.x,
+            top:dets.y
+        })
+    })
+    
+    Shery.makeMagnet("#nav-part2 h4");
+}
+cursorAnimation();
