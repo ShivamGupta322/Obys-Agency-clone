11aaa6d7ca8f58853222939823a239294d3cd4ed
ShivamGupta322 <guptashivamsg02@gmail.com>
2024-07-16 11:32:05 +0530

    loader made successfully

---
+++ script.js
@@ -0,0 +1,39 @@
+
+var tl=gsap.timeline()
+tl.from(".line h1",{
+    y:150,
+    stagger:0.25,
+    duration:0.6,
+    delay:0.5
+})
+tl.from("#line1-part-1, .line h2",{
+    opacity:0,
+    onStart:function(){
+        var h5timer=document.querySelector("#line1-part1 h5")
+        var grow=0
+    setInterval(function(){
+    if(grow<100){
+        grow++
+        h5timer.innerHTML=grow++
+    }
+    else{
+        h5timer.innerHTML=grow
+    }
+},33)
+    }
+})
+tl.to("#loader",{
+    opacity:0,
+    duration:0.4,
+    delay:4
+})
+
+tl.from("#page1",{
+    y:1600,
+    opacity:0,
+    duratio:0.5, 
+    ease:Power4
+})
+tl.to("#loader",{
+    display:"none"
+})
